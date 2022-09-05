6a4a747e9adb7f8a8af6ea5124fa8ba6d42f5158
hilalaseli <112526608+ilalaseli@users.noreply.github.com>
2022-09-04 18:29:15 -0700

    Add files via upload

---
+++ Python5-TKJ 4.py
@@ -0,0 +1,20 @@
+# Konversi mata uang
+print("mata uang dolar ke rupiah")
+dolar = int(input("dolar = "))
+
+rupiah = dolar * 14.897
+print ("rupiah=", rupiah)
+
+# Konversi euro ke yen
+print("mata uang euro ke yen")
+euro = int(input("euro = "))
+
+yen = euro * 139.16
+print ("yen=", yen)
+
+# Konversi won ke rupiah
+print("mata uang won ke rupiah")
+won = int(input("won = "))
+
+rupiah = won * 10.92
+print ("rupiah=", rupiah)
