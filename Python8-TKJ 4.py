bde4f44e5f0b8d9b3a847b8b29acbf8e5060f46d
hilalaseli <112526608+ilalaseli@users.noreply.github.com>
2022-09-06 19:18:13 -0700

    Add files via upload

---
+++ Python8-TKJ 4.py
@@ -0,0 +1,15 @@
+list_integer = [1, 2, 3, 10, 100, 250]
+list_string = ["Rifky", "Fajri", "Apran buzzcut", "Lulu penuh"]
+list_mix = [20, "subhan", "asep", "dahlan"]
+
+print("Data sebelum diubah:", list_string)
+list_string[0] = "Fadil bobo"
+list_string[1] = "angga ugly bastard"
+print("Data setelah diubah:", list_string)
+
+print("Data seblum diubah:", list_mix)
+list_mix[0] = 30
+list_mix[1] = "aceng"
+list_mix[2] = "kaori cicak"
+list_mix[3] = "komi linux"
+print("Data setelah diubah:", list_mix)
