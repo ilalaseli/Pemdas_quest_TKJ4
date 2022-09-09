4dd330a7b2ebe96c7d23b4b8ba2f36272a7f0ce8
hilalaseli <112526608+ilalaseli@users.noreply.github.com>
2022-09-09 00:47:02 -0700

    Add files via upload

---
+++ UPRAK_Muhammad Hilal Kamil_X TKJ 4.py
@@ -0,0 +1,39 @@
+# menghitung volume balok
+
+pb = 10
+lb = 15
+tb = 7
+luas_balok = pb * lb * tb
+print("volume balok adalah", luas_balok)
+
+# menghitung volume kubus
+
+sisi1 = 20
+sisi2 = 20
+sisi3 = 20
+luas_k = sisi1 * sisi2 * sisi3
+print("volume kubus adalah", luas_k)
+
+# menghitung volume limas
+ 
+p = 0.5
+la = 24
+t = 35
+jumlah_plat = 0.5 * 24 * 35
+print("volume limas adalah", jumlah_plat)
+
+# menghitung volume tabung
+
+phi = 22/7
+r1 = 5
+r2 = 5
+t = 7
+volume_tb = 22/7 * 5 * 5 * 7
+print("volume tabung adalah", volume_tb)
+
+# mengubah celcius ke reamur
+
+c = 20
+r = 4/5
+ubah_ctr = 4/5 * 20
+print("jumlah celcius ke reamur", ubah_ctr)
