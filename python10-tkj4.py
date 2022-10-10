9fd14483362b13e24bf2c47aeb1555b72f68ace6
hilalaseli <112526608+ilalaseli@users.noreply.github.com>
2022-10-09 19:43:44 -0700

    Add files via upload

---
+++ python10-tkj4.py
@@ -0,0 +1,29 @@
+#tipe data koleksi
+
+koleksi_data_str = ["suginem warteg", "samsul arip", "ferdi dowhill", "alibapa icikiwir"]
+
+koleksi_data_int = [200, 300, 400, 500]
+
+koleksi_data_mix = ["rusdi batagor", 400, True, "agung bismarck"]
+
+print("Koleksi data String:", koleksi_data_str)
+
+print("Koleksi data Integer:", koleksi_data_int)
+
+print("Koleksi data Campuran:", koleksi_data_mix)
+
+
+
+# Buatlah 3 kumpulan data: Nama Hewan, Nilai UTS, Nama Teman sebangku kalian
+
+nama_nama_hewan = ("klomang", "rodriguez", "kelinci", "icikiwir")
+
+list_nilai_uts = (80,90,100,95)
+
+nama_teman_sebangku = ("apran hololaip", "rifki kodoksan", "angga stlouis", "ilal belfast")
+
+print("nama nama hewan:", nama_nama_hewan)
+
+print("nilai uts saya:", list_nilai_uts)
+
+print("nama teman sebangku:", nama_teman_sebangku)
