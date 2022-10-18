3ef0a1d436a9c6d89e0fa6cfa93e0af6a2a4352e
hilalaseli <112526608+ilalaseli@users.noreply.github.com>
2022-10-17 19:44:26 -0700

    Add files via upload

---
+++ Python 11-TKJ 4.py
@@ -0,0 +1,22 @@
+gender = ("Laki Laki", "Perempuan")
+
+tuple_name =tuple(["Cahyadi","Rifky Basketball","ilal gim kapal"])
+
+tuple_tunggal = ("Apran hololaip")
+
+gabungan = gender + tuple_name
+
+print(gabungan)
+print(tuple_tunggal)
+
+# Buatlah 3 koleksi Tuple
+
+teman = ("angga tambel ban", "apran hololaip", "rifqi keong", "ilal meriam kapal")
+
+game = ("ajurlen", "gensin imfek", "tower op pentasi", "blu arcip")
+
+hewan = ("kodok", "keong", "cicak", "kelinci")
+
+print(teman)
+print(game)
+print(hewan)
